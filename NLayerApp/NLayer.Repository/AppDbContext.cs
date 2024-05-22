b69fde5caea9933165914188885fc70c014fc6ae
omerfaruksunar <omerfaruksunar@gmail.com>
2024-05-22 18:20:32 +0300

    Creating layers

---
+++ NLayerApp/NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Repository.Configurations;
+using System.Reflection;
+
+namespace NLayer.Repository;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+        {
+            Id = 1,
+            Color ="Kırmızı",
+            Height=100,
+            Width = 200,
+            ProductId = 1
+        },
+        new ProductFeature()
+        {
+            Id = 2,
+            Color = "Mavi",
+            Height = 300,
+            Width = 500,
+            ProductId = 2
+        });
+    }
+}
